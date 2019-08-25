eeb55a048f34c6857797cca6f8df71ad4f9a70c3
Lance Bendo <lunchbendo@gmail.com>
2019-08-25 12:40:24 -0700

    checkpoint uleeeeeet

--- src/services/web/routes/service.js
+++ src/services/web/routes/service.js
@@ -7,12 +7,20 @@ const router = express.Router();
 
 // GET /service?{filter} (get active services by filter. PUBLIC)
 router.get('/', (req, res, next) => {
-  next();
+  const queryString = `SELECT * FROM service ${getWhere({ is_active: 1, ...req.query })} ORDER BY CREATED_DATE DESC`;
+
+  connectWrapper({ isReadOnlyConnection: true })
+    .then(({ connection }) => queryWrapper(connection, queryString))
+    .then(({ connection, result }) => {
+      connection.end();
+      res.status(200).json({ status: 'success', data: result[0] });
+    })
+    .catch(({ error }) => next(error));
 });
 
 // GET /service/{id} (get service by id. PUBLIC)
 router.get('/:id(\\d+)', (req, res, next) => {
-  const queryString = `SELECT * FROM service ${getWhere({ id: req.params.id })} LIMIT 1`;
+  const queryString = `SELECT * FROM service ${getWhere({ is_active: 1, id: req.params.id })} LIMIT 1`;
 
   connectWrapper({ isReadOnlyConnection: true })
     .then(({ connection }) => queryWrapper(connection, queryString))
@@ -45,13 +53,45 @@ router.post('/', (req, res, next) => {
 });
 
 // PUT /service/{id} (update service. ADMIN ONLY)
-router.put('/:id/', (req, res, next) => {
-  next();
+router.put('/:id(\\d+)', (req, res, next) => {
+  const { name, description } = req.body;
+  const { id } = req.params;
+
+  connectWrapper({ isReadOnlyConnection: false, isTransaction: true })
+    .then(({ connection }) => queryWrapper(
+      connection,
+      'CALL serviceUpdate(?, ?, ?)',
+      [id, name, description],
+    ))
+    .then(({ connection }) => queryWrapper(connection, `SELECT * FROM service ${getWhere({ id })}`))
+    .then(({ connection, result }) => {
+      connection.end();
+      res.status(201).json({ status: 'success', data: result[0] });
+    })
+    .catch(({ connection, error }) => {
+      connection.rollback();
+      next(error);
+    });
 });
 
 // DELETE /service/{id} (disable service. ADMIN ONLY)
-router.delete('/:id/', (req, res, next) => {
-  next();
+router.delete('/:id(\\d+)', (req, res, next) => {
+  const { id } = req.params;
+
+  connectWrapper({ isReadOnlyConnection: false, isTransaction: true })
+    .then(({ connection }) => queryWrapper(
+      connection,
+      'CALL isActiveUpdate(?, ?, ?)',
+      ['service', id, 0],
+    ))
+    .then(({ connection }) => {
+      connection.end();
+      res.status(201).json({ status: 'success', data: { deletedId: id } });
+    })
+    .catch(({ connection, error }) => {
+      connection.rollback();
+      next(error);
+    });
 });
 
 export default router;
